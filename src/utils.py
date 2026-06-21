08ee39599dad3afaff2a92ac7484de1f7a2194dc
Muhammad Shahab <humiliy3579@gmail.com>
2026-06-21 15:21:12 +0500

    Added

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 import dill
-from src.exception import CustomException
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 
 def save_object(file_path,obj):
     try:
@@ -53,3 +55,11 @@ def evaluate_models (X_train, y_train,X_test,y_test, models , param):
             return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
